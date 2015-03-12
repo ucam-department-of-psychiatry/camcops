8b843052abc992f03701984daf7282147ac04c38
Rudolf Cardinal <rudolf@pobox.com>
2015-03-12 13:10:38 +0000

    First commit

---
+++ src/org/camcops/androidtibugfix/TextAreaProxy.java
@@ -0,0 +1,123 @@
+// https://raw.github.com/appcelerator/titanium_mobile/master/android/modules/ui/src/java/ti/modules/titanium/ui/TextAreaProxy.java
+
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package org.camcops.androidtibugfix; // RNC // package ti.modules.titanium.ui;
+
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiC;
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.proxy.TiViewProxy;
+import org.appcelerator.titanium.util.TiConvert;
+import org.appcelerator.titanium.view.TiUIView;
+
+// import org.camcops.androidtibugfix.TiUIText; // RNC // import ti.modules.titanium.ui.widget.TiUIText;
+import android.app.Activity;
+import android.os.Message;
+
+@Kroll.proxy(creatableInModule=AndroidtibugfixModule.class, propertyAccessors = { // RNC: change of module
+	TiC.PROPERTY_AUTOCAPITALIZATION,
+	TiC.PROPERTY_AUTOCORRECT,
+	TiC.PROPERTY_AUTO_LINK,
+	TiC.PROPERTY_CLEAR_ON_EDIT,
+	TiC.PROPERTY_COLOR,
+	TiC.PROPERTY_EDITABLE,
+	TiC.PROPERTY_ELLIPSIZE,
+	TiC.PROPERTY_ENABLE_RETURN_KEY,
+	TiC.PROPERTY_FONT,
+	TiC.PROPERTY_HINT_TEXT,
+	TiC.PROPERTY_KEYBOARD_TYPE,
+	TiC.PROPERTY_MAX_LENGTH,
+	TiC.PROPERTY_PASSWORD_MASK,
+	TiC.PROPERTY_TEXT_ALIGN,
+	TiC.PROPERTY_VALUE,
+	TiC.PROPERTY_VERTICAL_ALIGN,
+	TiC.PROPERTY_RETURN_KEY_TYPE
+})
+public class TextAreaProxy extends TiViewProxy
+{
+	private static final int MSG_FIRST_ID = TiViewProxy.MSG_LAST_ID + 1;
+	private static final int MSG_SET_SELECTION = MSG_FIRST_ID + 201;
+
+	public TextAreaProxy()
+	{
+		super();
+		defaultValues.put(TiC.PROPERTY_VALUE, "");
+		defaultValues.put(TiC.PROPERTY_MAX_LENGTH, -1);
+	}
+
+	public TextAreaProxy(TiContext tiContext)
+	{
+		this();
+	}
+
+	@Override
+	public void handleCreationArgs(KrollModule createdInModule, Object[] args)
+	{
+		super.handleCreationArgs(createdInModule, args);
+
+	}
+
+	@Override
+	public TiUIView createView(Activity activity)
+	{
+		return new TiUIText(this, false);
+	}
+	
+	@Kroll.method
+	public Boolean hasText()
+	{
+		Object text = getProperty(TiC.PROPERTY_VALUE);
+		return (TiConvert.toString(text, "").length() > 0);
+	}
+	
+	@Kroll.method
+	public void setSelection(int start, int stop)
+	{
+		TiUIView v = getOrCreateView();
+		if (v != null) {
+			if (TiApplication.isUIThread()) {
+				((TiUIText)v).setSelection(start, stop);
+				return;
+			}
+			KrollDict args = new KrollDict();
+			args.put(TiC.PROPERTY_START, start);
+			args.put(TiC.PROPERTY_STOP, stop);
+			getMainHandler().obtainMessage(MSG_SET_SELECTION, args).sendToTarget();
+		}
+	}
+	
+	public boolean handleMessage(Message msg)
+	{
+		switch (msg.what) {
+			case MSG_SET_SELECTION: {
+				TiUIView v = getOrCreateView();
+				if (v != null) {
+					Object argsObj = msg.obj;
+					if (argsObj instanceof KrollDict) {
+						KrollDict args = (KrollDict) argsObj;
+						((TiUIText)v).setSelection(args.getInt(TiC.PROPERTY_START), args.getInt(TiC.PROPERTY_STOP));
+					}
+				}
+				return true;
+			}
+			
+			default: {
+				return super.handleMessage(msg);
+			}
+		}
+	}
+
+	@Override
+	public String getApiName()
+	{
+		return "Ti.UI.TextArea";
+	}
+}
