4f5485f77f67e68ffbf1fbea4dbb135f35defc43
Rudolf Cardinal <rudolf@pobox.com>
2016-01-08 00:07:56 +0000

    script change

--- server/tools/launch_dev_server.sh
+++ server/tools/launch_dev_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+THIS_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd "$THIS_SCRIPT_DIR/.."
+
 gunicorn camcops:application \
     --env CAMCOPS_CONFIG_FILE=/etc/camcops/camcops_py3_test.conf \
     --workers 2 \
