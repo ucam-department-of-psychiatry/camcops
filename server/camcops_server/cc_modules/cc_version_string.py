1f0447da3feb9bcd45e758c60ebce205cc4d5f6e
Rudolf Cardinal <rudolf@pobox.com>
2017-01-28 16:51:26 +0000

    semantic versions

---
+++ server/camcops_server/cc_modules/cc_version_string.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# cc_version_string.py
+
+"""
+===============================================================================
+    Copyright (C) 2012-2017 Rudolf Cardinal (rudolf@pobox.com).
+
+    This file is part of CamCOPS.
+
+    CamCOPS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CamCOPS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CamCOPS. If not, see <http://www.gnu.org/licenses/>.
+===============================================================================
+"""
+
+# This file MAY NOT import anything except the standard library, because it
+# is read by setup.py, which must operate in a base Python environment.
+
+# =============================================================================
+# Version constants and configuration variables read by shell scripts
+# =============================================================================
+
+CAMCOPS_SERVER_VERSION_STRING = "2.0.0"
+CAMCOPS_CHANGEDATE = "2016-12-06"
+MINIMUM_TABLET_VERSION_STRING = "1.14.0"
