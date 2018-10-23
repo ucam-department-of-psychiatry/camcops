68289ffa80d1d6d306e09c9b3fad961daaacd20d
Joe Kearney <jngk2@cam.ac.uk>
2018-10-23 09:26:01 +0100

    Initial eq5d5l task commit.

---
+++ tablet_qt/tasks/eq5d5l.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <QPointer>
+#include <QString>
+#include "tasklib/task.h"
+
+class CamcopsApp;
+class OpenableWidget;
+class Questionnaire;
+class TaskFactory;
+
+void initializeEq5d5l(TaskFactory& factory);
+
+class Eq5d5l : public Task
+{
+    Q_OBJECT
+public:
+    Eq5d5l(CamcopsApp& app, DatabaseManager& db,
+         int load_pk = dbconst::NONEXISTENT_PK);
+    // ------------------------------------------------------------------------
+    // Class overrides
+    // ------------------------------------------------------------------------
+    virtual QString shortname() const override;
+    virtual QString longname() const override;
+    virtual QString menusubtitle() const override;
+    virtual Version minimumServerVersion() const override;
+    // ------------------------------------------------------------------------
+    // Instance overrides
+    // ------------------------------------------------------------------------
+    virtual bool isComplete() const override;
+    virtual QStringList summary() const override;
+    virtual QStringList detail() const override;
+    virtual OpenableWidget* editor(bool read_only = false) override;
+    // ------------------------------------------------------------------------
+    // Task-specific calculations
+    // ------------------------------------------------------------------------
+protected slots:
+
+public:
+    static const QString EQ5D5L_TABLENAME;
+protected:
+    bool m_in_tickbox_change;
+};
