54e9118c03ce0da87693b6cf4c1a5b9d1e81e57c
Martin Burchell <mb2353@cam.ac.uk>
2019-09-17 15:24:16 +0100

    Fix another couple of missing template variables

--- server/camcops_server/cc_modules/webview.py
+++ server/camcops_server/cc_modules/webview.py
@@ -2896,7 +2896,8 @@ def add_special_note(req: "CamcopsRequest") -> Dict[str, Any]:
         rendered_form = form.render(appstruct)
     return dict(task=task,
                 form=rendered_form,
-                head_form_html=get_head_form_html(req, [form]))
+                head_form_html=get_head_form_html(req, [form]),
+                viewtype=ViewArg.HTML)
 
 
 @view_config(route_name=Routes.DELETE_SPECIAL_NOTE,
@@ -3005,7 +3006,8 @@ def erase_task(req: "CamcopsRequest") -> Response:
         dict(
             task=task,
             form=rendered_form,
-            head_form_html=get_head_form_html(req, [form])
+            head_form_html=get_head_form_html(req, [form]),
+            viewtype=ViewArg.HTML
         ),
         request=req
     )
