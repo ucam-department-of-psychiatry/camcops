b75e84f28ff326c78913a5408321ef174140aa31
Martin Burchell <mb2353@cam.ac.uk>
2019-09-24 17:50:14 +0100

    Initial work on the APEQ CPFT Perinatal report

--- server/camcops_server/tasks/apeq_cpft_perinatal.py
+++ server/camcops_server/tasks/apeq_cpft_perinatal.py
@@ -28,11 +28,17 @@ camcops_server/tasks/apeq_cpft_perinatal.py
 
 from typing import Dict, List, Optional
 
+from cardinal_pythonlib.classes import classproperty
+
+from pyramid.renderers import render_to_response
+from pyramid.response import Response
+from sqlalchemy.sql.expression import and_, column, func, select
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import Integer, UnicodeText
 
 from camcops_server.cc_modules.cc_constants import CssClass
 from camcops_server.cc_modules.cc_html import tr_qa
+from camcops_server.cc_modules.cc_report import Report
 from camcops_server.cc_modules.cc_request import CamcopsRequest
 from camcops_server.cc_modules.cc_task import Task
 
@@ -138,3 +144,181 @@ class APEQCPFTPerinatal(Task):
                        self.comments or "")}
             </table>
         """
+
+    def get_ff_options(self, req: "CamcopsRequest") -> List[str]:
+        options = []
+
+        for n in range(0, 5 + 1):
+            options.append(self.wxstring(req, f"ff_a{n}"))
+
+        return options
+
+
+# =============================================================================
+# Reports
+# =============================================================================
+
+class APEQCPFTPerinatalReport(Report):
+    """
+    Provides a summary of each question, x% of people said each response etc.
+    Then a summary of the comments.
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.task = APEQCPFTPerinatal()
+
+    @classproperty
+    def report_id(cls) -> str:
+        return "apeq_cpft_perinatal"
+
+    @classmethod
+    def title(cls, req: "CamcopsRequest") -> str:
+        _ = req.gettext
+        return _("APEQ CPFT Perinatal — Question summaries")
+
+    @classproperty
+    def superuser_only(cls) -> bool:
+        return False
+
+    def get_response(self, req: "CamcopsRequest") -> Response:
+        return render_to_response(
+            "apeq_cpft_perinatal_report.mako",
+            dict(
+                title=self.title(req),
+                report_id=self.report_id,
+                main_column_headings=self._get_main_column_headings(req),
+                main_rows=self._get_main_rows(req),
+                ff_column_headings=self._get_ff_column_headings(req),
+                ff_rows=self._get_ff_rows(req),
+                ff_why_rows=self._get_ff_why_rows(req),
+                comments=self._get_comments(req)
+            ),
+            request=req
+        )
+
+    def _get_main_column_headings(self, req: "CamcopsRequest") -> List[str]:
+        _ = req.gettext
+        names = [_("Question")]
+
+        for n in range(0, 2 + 1):
+            names.append(self.task.wxstring(req, f"main_a{n}"))
+
+        return names
+
+    def _get_main_rows(self, req: "CamcopsRequest") -> List[str]:
+        """
+        Percentage of people who answered x for each question
+        """
+        column_dict = {}
+
+        qnums = range(self.task.FIRST_MAIN_Q, self.task.LAST_MAIN_Q + 1)
+
+        for qnum in qnums:
+            column_name = f"{self.task.FN_QPREFIX}{qnum}"
+
+            column_dict[column_name] = self.task.wxstring(req, column_name)
+
+        return self._get_response_percentages(
+            req,
+            column_dict=column_dict,
+            num_answers=3
+        )
+
+    def _get_ff_column_headings(self, req: "CamcopsRequest") -> List[str]:
+        _ = req.gettext
+        return [_("Question")] + self.task.get_ff_options(req)
+
+    def _get_ff_rows(self, req: "CamcopsRequest") -> List[str]:
+        """
+        Percentage of people who answered x for the friends/family question
+        """
+        return self._get_response_percentages(
+            req,
+            column_dict={
+                "ff_rating": self.task.wxstring(
+                    req,
+                    f"{self.task.FN_QPREFIX}_ff_rating"
+                )
+            },
+            num_answers=6
+        )
+
+    def _get_ff_why_rows(self, req: "CamcopsRequest") -> List[str]:
+        """
+        Reasons for giving a particular answer to the friends/family question
+        """
+
+        options = self.task.get_ff_options(req)
+
+        query = (
+            select([
+                column("ff_rating"),
+                column("ff_why")
+            ])
+            .select_from(self.task.__table__)
+            .where(
+                and_(
+                    column("ff_rating").isnot(None),
+                    column("ff_why").isnot(None)
+                )
+            )
+            .order_by("ff_why")
+        )
+
+        rows = []
+
+        for result in req.dbsession.execute(query).fetchall():
+            rows.append([options[result[0]], result[1]])
+
+        return rows
+
+    def _get_comments(self, req: "CamcopsRequest") -> List[str]:
+        """
+        A list of all the additional comments
+        """
+
+        query = (
+            select([
+                column("comments"),
+            ])
+            .select_from(self.task.__table__)
+            .where(
+                column("comments").isnot(None)
+            )
+        )
+
+        return req.dbsession.execute(query).fetchall()
+
+    def _get_response_percentages(self,
+                                  req: "CamcopsRequest",
+                                  column_dict: Dict[str, str],
+                                  num_answers: int) -> List[str]:
+
+        rows = []
+
+        for column_name, question in column_dict.items():
+            total_query = (
+                select([func.count(column_name)])
+                .select_from(self.task.__table__)
+                .where(column(column_name).isnot(None))
+            )
+
+            row = [question] + [0] * num_answers
+
+            query = (
+                select([
+                    column(column_name),
+                    100*(func.count(column_name)/total_query)
+                ])
+                .select_from(self.task.__table__)
+                .where(column(column_name).isnot(None))
+                .group_by(column_name)
+            )
+
+            for result in req.dbsession.execute(query):
+                row[result[0]+1] = result[1]
+
+            rows.append(row)
+
+        return rows
