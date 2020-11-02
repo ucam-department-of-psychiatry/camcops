7eca7e84c2869876957a6833c1fd486486adbf9a
Matthew Moore <mm2439@medschl.cam.ac.uk>
2020-11-02 16:37:57 +0000

    Updating eigen download location from Bitbucket to GitLab

--- tablet_qt/tools/build_qt.py
+++ tablet_qt/tools/build_qt.py
@@ -1581,10 +1581,12 @@ class Config(object):
         self.sqlcipher_src_gitdir = join(self.src_rootdir, "sqlcipher")  # type: str  # noqa
 
         # Eigen
+        # Changed location from bitbucket - https://gitlab.com/libeigen/eigen/-/archive/3.3.8/eigen-3.3.8.tar.gz
         self.eigen_version = EIGEN_VERSION  # type: str
         self.eigen_src_url = (
-            f"http://bitbucket.org/eigen/eigen/get/{self.eigen_version}.tar.gz"  # noqa
+            f"https://gitlab.com/libeigen/eigen/-/archive/{self.eigen_version}/eigen-{self.eigen_version}.tar.gz"  # noqa
         )
+
         self.eigen_src_dir = join(self.src_rootdir, "eigen")
         self.eigen_src_fullpath = join(
             self.eigen_src_dir,
