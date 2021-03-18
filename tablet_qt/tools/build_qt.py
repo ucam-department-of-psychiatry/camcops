f482f1e33e43b60dad0a9efaa5ded6ee05ccbd08
Martin Burchell <mb2353@cam.ac.uk>
2021-03-18 14:59:07 +0000

    WIP fix for Windows Qt build

--- tablet_qt/tools/build_qt.py
+++ tablet_qt/tools/build_qt.py
@@ -3128,7 +3128,14 @@ def build_qt(cfg: Config, target_platform: Platform) -> str:
     # Qt: Environment
     # -------------------------------------------------------------------------
     env = cfg.get_starting_env()
-    openssl_libs = f"-L{openssl_lib_root} -lssl -lcrypto -ldl -lpthread"
+    crypto_dependencies = ""
+    if target_platform.linux:
+        crypto_dependencies = "-ldl -lpthread"
+    elif target_platform.windows:
+        crypto_dependencies = "-lcrypt32"
+
+    openssl_libs = f"-L{openssl_lib_root} -lssl -lcrypto {crypto_dependencies}"
+
     # See also https://bugreports.qt.io/browse/QTBUG-62016
     env['OPENSSL_LIBS'] = openssl_libs
     # Setting OPENSSL_LIBS as an *environment variable* may be unnecessary,
