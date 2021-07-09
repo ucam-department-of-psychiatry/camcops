6b76eb8209d30ff2ec7359aaca5216a05db64812
Martin Burchell <mb2353@cam.ac.uk>
2021-07-09 10:44:18 +0100

    v2.4.8 version bumps

--- tablet_qt/version/camcopsversion.cpp
+++ tablet_qt/version/camcopsversion.cpp
@@ -26,8 +26,8 @@ namespace camcopsversion {  // http://semver.org/
 // ----------------------------------------------------------------------------
 // Increase this when the client is changed:
 
-const Version CAMCOPS_CLIENT_VERSION(2, 4, 6);
-const QDate CAMCOPS_CLIENT_CHANGEDATE(2021, 5, 7);
+const Version CAMCOPS_CLIENT_VERSION(2, 4, 8);
+const QDate CAMCOPS_CLIENT_CHANGEDATE(2021, 7, 9);
 
 // +++ NOW ALSO UPDATE: +++
 //
