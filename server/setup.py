73acf7c9455cf7cc3ce443dd137a2594df54cdbf
Martin Burchell <mb2353@cam.ac.uk>
2021-08-24 12:06:28 +0100

    2FA: Add phonenumbers package

--- server/setup.py
+++ server/setup.py
@@ -104,6 +104,7 @@ INSTALL_REQUIRES = [
     'pendulum==2.1.2',  # date/time classes
     'pexpect==4.8.0',  # for open_sqlcipher.py
     'pdfkit==0.6.1',  # wkhtmltopdf interface, for PDF generation from HTML
+    'phonenumbers==8.12.30',  # phone number parsing, storing and validating
     'py==1.10.0',  # dependency, pinned to avoid CVE-2020-29651
     'pycap==1.1.1',  # REDCap integration
     'Pygments==2.7.4',  # Syntax highlighting for introspection/DDL
