95af9666178d75616ffb3861dad461844fea2058
Martin Burchell <mb2353@cam.ac.uk>
2022-01-04 11:57:51 +0000

    Bump AMQP for Celery dependency

--- server/setup.py
+++ server/setup.py
@@ -180,7 +180,7 @@ INSTALL_REQUIRES = [
     # -------------------------------------------------------------------------
 
     'alabaster==0.7.12',  # for sphinx
-    'amqp==5.0.6',  # for celery
+    'amqp==5.0.9',  # for celery
     'Chameleon==3.8.1',  # for Deform
     'tornado==6.1',  # for celery
     'webob==1.8.6',  # for pyramid
