df9449fe70ce141c8c3fc214149780dee46941dd
Martin Burchell <mb2353@cam.ac.uk>
2022-03-08 21:38:11 +0000

    Qt 6.2: Update SSL Protocols

--- tablet_qt/lib/convert.cpp
+++ tablet_qt/lib/convert.cpp
@@ -816,18 +816,23 @@ QUrlQuery getPostDataAsUrlQuery(const QMap<QString, QString>& dict)
 }
 
 
-// http://doc.qt.io/qt-5/qssl.html#SslProtocol-enum
-const QString SSLPROTODESC_SSLV3 = "SslV3";
-const QString SSLPROTODESC_SSLV2 = "SslV2";
+// https://doc.qt.io/qt-6/qssl.html#SslProtocol-enum
 const QString SSLPROTODESC_TLSV1_0 = "TlsV1_0";
-const QString SSLPROTODESC_TLSV1_1 = "TlsV1_1";
-const QString SSLPROTODESC_TLSV1_2 = "TlsV1_2";
-const QString SSLPROTODESC_ANYPROTOCOL = "AnyProtocol";
-const QString SSLPROTODESC_TLSV1_SSLV3 = "TlsV1SslV3";
-const QString SSLPROTODESC_SECUREPROTOCOLS = "SecureProtocols";
 const QString SSLPROTODESC_TLSV1_0_OR_LATER = "TlsV1_0OrLater";
+const QString SSLPROTODESC_TLSV1_1 = "TlsV1_1";
 const QString SSLPROTODESC_TLSV1_1_OR_LATER = "TlsV1_1OrLater";
+const QString SSLPROTODESC_TLSV1_2 = "TlsV1_2";
 const QString SSLPROTODESC_TLSV1_2_OR_LATER = "TlsV1_2OrLater";
+const QString SSLPROTODESC_DTLSV1_0 = "DtlsV1_0";
+const QString SSLPROTODESC_DTLSV1_0_OR_LATER = "DtlsV1_0OrLater";
+const QString SSLPROTODESC_DTLSV1_1 = "DtlsV1_1";
+const QString SSLPROTODESC_DTLSV1_1_OR_LATER = "DtlsV1_1OrLater";
+const QString SSLPROTODESC_DTLSV1_2 = "DtlsV1_2";
+const QString SSLPROTODESC_DTLSV1_2_OR_LATER = "DtlsV1_2OrLater";
+const QString SSLPROTODESC_TLSV1_3 = "TlsV1_3";
+const QString SSLPROTODESC_TLSV1_3_OR_LATER = "TlsV1_3OrLater";
+const QString SSLPROTODESC_ANYPROTOCOL = "AnyProtocol";
+const QString SSLPROTODESC_SECUREPROTOCOLS = "SecureProtocols";
 const QString SSLPROTODESC_UNKNOWN_PROTOCOL = "UnknownProtocol";
 
 
@@ -835,20 +840,20 @@ QString describeSslProtocol(const QSsl::SslProtocol protocol)
 {
     using namespace QSsl;
     switch (protocol) {
-    case SslV3: return SSLPROTODESC_SSLV3;
-    case SslV2: return SSLPROTODESC_SSLV2;
     case TlsV1_0: return SSLPROTODESC_TLSV1_0;
-#if QT_DEPRECATED_SINCE(5,0)
-    case TlsV1: return TLSV1_0;
-#endif
-    case TlsV1_1: return SSLPROTODESC_TLSV1_1;
-    case TlsV1_2: return SSLPROTODESC_TLSV1_2;
-    case AnyProtocol: return SSLPROTODESC_ANYPROTOCOL;
-    case TlsV1SslV3: return SSLPROTODESC_TLSV1_SSLV3;
-    case SecureProtocols: return SSLPROTODESC_SECUREPROTOCOLS;
     case TlsV1_0OrLater: return SSLPROTODESC_TLSV1_0_OR_LATER;
+    case TlsV1_1: return SSLPROTODESC_TLSV1_1;
     case TlsV1_1OrLater: return SSLPROTODESC_TLSV1_1_OR_LATER;
+    case TlsV1_2: return SSLPROTODESC_TLSV1_2;
     case TlsV1_2OrLater: return SSLPROTODESC_TLSV1_2_OR_LATER;
+    case DtlsV1_0: return SSLPROTODESC_DTLSV1_0;
+    case DtlsV1_0OrLater: return SSLPROTODESC_DTLSV1_0_OR_LATER;
+    case DtlsV1_2: return SSLPROTODESC_DTLSV1_2;
+    case DtlsV1_2OrLater: return SSLPROTODESC_DTLSV1_2_OR_LATER;
+    case TlsV1_3: return SSLPROTODESC_TLSV1_3;
+    case TlsV1_3OrLater: return SSLPROTODESC_TLSV1_3_OR_LATER;
+    case AnyProtocol: return SSLPROTODESC_ANYPROTOCOL;
+    case SecureProtocols: return SSLPROTODESC_SECUREPROTOCOLS;
     default:
     case UnknownProtocol: return SSLPROTODESC_UNKNOWN_PROTOCOL;
     }
@@ -858,17 +863,20 @@ QString describeSslProtocol(const QSsl::SslProtocol protocol)
 QSsl::SslProtocol sslProtocolFromDescription(const QString& desc)
 {
     using namespace QSsl;
-    if (desc == SSLPROTODESC_SSLV3) return SslV3;
-    if (desc == SSLPROTODESC_SSLV2) return SslV2;
     if (desc == SSLPROTODESC_TLSV1_0) return TlsV1_0;
-    if (desc == SSLPROTODESC_TLSV1_1) return TlsV1_1;
-    if (desc == SSLPROTODESC_TLSV1_2) return TlsV1_2;
-    if (desc == SSLPROTODESC_ANYPROTOCOL) return AnyProtocol;
-    if (desc == SSLPROTODESC_TLSV1_SSLV3) return TlsV1SslV3;
-    if (desc == SSLPROTODESC_SECUREPROTOCOLS) return SecureProtocols;
     if (desc == SSLPROTODESC_TLSV1_0_OR_LATER) return TlsV1_0OrLater;
+    if (desc == SSLPROTODESC_TLSV1_1) return TlsV1_1;
     if (desc == SSLPROTODESC_TLSV1_1_OR_LATER) return TlsV1_1OrLater;
+    if (desc == SSLPROTODESC_TLSV1_2) return TlsV1_2;
     if (desc == SSLPROTODESC_TLSV1_2_OR_LATER) return TlsV1_2OrLater;
+    if (desc == SSLPROTODESC_DTLSV1_0) return DtlsV1_0;
+    if (desc == SSLPROTODESC_DTLSV1_0_OR_LATER) return DtlsV1_0OrLater;
+    if (desc == SSLPROTODESC_DTLSV1_2) return DtlsV1_2;
+    if (desc == SSLPROTODESC_DTLSV1_2_OR_LATER) return DtlsV1_2OrLater;
+    if (desc == SSLPROTODESC_TLSV1_3) return TlsV1_3;
+    if (desc == SSLPROTODESC_TLSV1_3_OR_LATER) return TlsV1_3OrLater;
+    if (desc == SSLPROTODESC_ANYPROTOCOL) return AnyProtocol;
+    if (desc == SSLPROTODESC_SECUREPROTOCOLS) return SecureProtocols;
     return UnknownProtocol;
 }
 
