b16b50fbc681482a099de8f505981011eae3e04f
Martin Burchell <mb2353@cam.ac.uk>
2022-03-11 21:04:47 +0000

    Qt 6.2: I meant f.read()

--- tablet_qt/tools/build_qt.py
+++ tablet_qt/tools/build_qt.py
@@ -3419,14 +3419,14 @@ Troubleshooting Qt 'configure' failures
             output_log = os.path.join(builddir, "CMakeFiles",
                                       "CMakeOutput.log")
             with open(output_log) as f:
-                log.warning(f.read)
+                log.warning(f.read())
 
             log.warning(80 * "*")
             log.warning("Contents of CMakeError.log:")
             error_log = os.path.join(builddir, "CMakeFiles",
                                      "CMakeError.log")
             with open(error_log) as f:
-                log.warning(f.read)
+                log.warning(f.read())
 
             sys.exit(EXIT_FAILURE)
 
