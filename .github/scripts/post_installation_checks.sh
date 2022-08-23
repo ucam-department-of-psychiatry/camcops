e9bb7c505ec11d071cffe1d67453c6981f5fc621
Martin Burchell <mb2353@cam.ac.uk>
2022-08-24 00:50:34 +0100

    Fix path in post-installation checks

--- .github/scripts/post_installation_checks.sh
+++ .github/scripts/post_installation_checks.sh
@@ -6,8 +6,8 @@
 set -euxo pipefail
 
 # Check server is running
-cd "${CAMCOPS_HOME}/docker/dockerfiles"
+cd "${CAMCOPS_HOME}/server/docker/dockerfiles"
 docker compose logs
 SERVER_IP=$(docker inspect camcops_camcops_server --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 wait-for-it "${SERVER_IP}:8000" --timeout=300
-curl -I -L --retry 10 --fail "${SERVER_IP}:8000/camcops/"
+curl -I -L --retry 10 --fail "${SERVER_IP}:8000/"
