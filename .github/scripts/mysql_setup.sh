170efd53e40fc48574827fcc5751e31ccfd4c31d
Martin Burchell <mb2353@cam.ac.uk>
2022-09-09 14:13:55 +0100

    Try to fix MySQL setup commands in installer action

--- .github/scripts/mysql_setup.sh
+++ .github/scripts/mysql_setup.sh
@@ -8,6 +8,6 @@ sudo apt -y install gnutls-bin
 sudo sed -i "s/^bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 cat /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql start
-mysql -e "CREATE DATABASE ${CAMCOPS_DOCKER_MYSQL_CAMCOPS_DATABASE_NAME};" -uroot -proot
-mysql -e "CREATE USER '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_NAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_PASSWORD}';" -uroot -proot
-mysql -e "GRANT ALL PRIVILEGES ON '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_DATABASE_NAME}'.* TO '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_NAME}'@'%';" -uroot -proot
+mysql --raw -e "CREATE DATABASE ${CAMCOPS_DOCKER_MYSQL_CAMCOPS_DATABASE_NAME};" -uroot -proot
+mysql --raw -e "CREATE USER '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_NAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_PASSWORD}';" -uroot -proot
+mysql --raw -e "GRANT ALL PRIVILEGES ON ${CAMCOPS_DOCKER_MYSQL_CAMCOPS_DATABASE_NAME}.* TO '${CAMCOPS_DOCKER_MYSQL_CAMCOPS_USER_NAME}'@'%';" -uroot -proot
