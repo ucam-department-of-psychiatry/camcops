61de594fa022eaa84e4935045e793d8bd68538d0
Martin Burchell <mb2353@cam.ac.uk>
2022-09-23 23:21:58 +0100

    Put back the right repository

--- server/installer/installer.sh
+++ server/installer/installer.sh
@@ -115,8 +115,7 @@ CAMCOPS_INSTALLER_PYTHON=${CAMCOPS_INSTALLER_PYTHON:-python3}
 # -----------------------------------------------------------------------------
 
 if [ ${PRODUCTION} -eq 1 ]; then
-    # CAMCOPS_GITHUB_REPOSITORY=https://github.com/ucam-department-of-psychiatry/camcops
-    CAMCOPS_GITHUB_REPOSITORY=https://github.com/martinburchell/camcops
+    CAMCOPS_GITHUB_REPOSITORY=https://github.com/ucam-department-of-psychiatry/camcops
     CAMCOPS_TAR_FILE=camcops_server.tar.gz
 
     # This doesn't work with GitHub generated assets for some reason so we rely
