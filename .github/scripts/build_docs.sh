b8222647b05cd4703174f2aaca8c2bfbec9dc283
Martin Burchell <mb2353@cam.ac.uk>
2022-11-25 18:45:52 +0000

    Maybe we didn't need to pin setuptools after all

--- .github/scripts/build_docs.sh
+++ .github/scripts/build_docs.sh
@@ -12,12 +12,8 @@ source "${HOME}/venv/bin/activate"
 python -VV
 python -m site
 python -m pip install -U pip
-# Until numpy is fixed
-# https://github.com/numpy/numpy/issues/22623
-python -m pip install "setuptools<60"
 echo installing pip packages
-cd "${GITHUB_WORKSPACE}/server"
-python -m pip install -e .
+python -m pip install -e server/.
 python -m pip install mysqlclient
 export CAMCOPS_CONFIG_FILE=${HOME}/camcops.cfg
 camcops_server demo_camcops_config > $CAMCOPS_CONFIG_FILE
