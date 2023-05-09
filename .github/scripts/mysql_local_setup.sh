9a2a91b547a28c2a380436058b951f8c148e037c
Martin Burchell <mb2353@cam.ac.uk>
2023-05-09 10:10:54 +0100

    Create mysql user with mysql_native_password in workflows

--- .github/scripts/mysql_local_setup.sh
+++ .github/scripts/mysql_local_setup.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 sudo systemctl start mysql
 mysql -e 'CREATE DATABASE camcops;' -uroot -proot
-mysql -e 'CREATE USER `camcops`@`localhost` IDENTIFIED BY "camcops";' -uroot -proot
+mysql -e 'CREATE USER `camcops`@`localhost` IDENTIFIED WITH mysql_native_password BY "camcops";' -uroot -proot
 mysql -e 'GRANT ALL PRIVILEGES ON `camcops`.* TO `camcops`@`localhost`;' -uroot -proot
