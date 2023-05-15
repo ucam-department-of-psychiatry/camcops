03cc323243a6b0713a164a95adb15b32ae42ed30
Martin Burchell <mb2353@cam.ac.uk>
2023-05-15 11:08:37 +0100

    Report where config file is being written in GitHub workflow.
    Because of the -u flag this will abort the script if CAMCOPS_CONFIG_FILE is undefined

--- .github/scripts/create_config_file.sh
+++ .github/scripts/create_config_file.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Because of -u above, this will abort the script if CAMCOPS_CONFIG_FILE is
+# undefined_
+echo "Writing configuration to ${CAMCOPS_CONFIG_FILE}"
 camcops_server demo_camcops_config | sed 's/YYY_USERNAME_REPLACE_ME/camcops/g' | sed 's/ZZZ_PASSWORD_REPLACE_ME/camcops/g' > "${CAMCOPS_CONFIG_FILE}"
