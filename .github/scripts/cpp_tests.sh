e20e3ba89ce7ea8230a53aac27273b9458e285ca
Martin Burchell <mb2353@cam.ac.uk>
2023-07-10 23:09:50 +0100

    Fix run command for setup-xvfb

---
+++ .github/scripts/cpp_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+export CAMCOPS_QT6_BASE_DIR=${RUNNER_WORKSPACE}
+cd ${CAMCOPS_QT6_BASE_DIR}
+mkdir -p eigen
+cd eigen
+EIGEN_VERSION=3.4.0
+wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_VERSION}/eigen-${EIGEN_VERSION}.tar.gz
+tar xvzf eigen-${EIGEN_VERSION}.tar.gz
+cd ${GITHUB_WORKSPACE}/tablet_qt/tests
+qmake
+make
+export QT_DEBUG_PLUGINS=1
+auto/questionnairelib/bin/test_questionnairelib
