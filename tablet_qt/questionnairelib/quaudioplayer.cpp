329837c7306bc74ee1eb4ee4e6ec39bea7627532
Martin Burchell <mb2353@cam.ac.uk>
2023-07-14 16:18:49 +0100

    Fix setting of volume in QuAudioPlayer
    Convert percent into proportion. Previously would be set at the maximum

--- tablet_qt/questionnairelib/quaudioplayer.cpp
+++ tablet_qt/questionnairelib/quaudioplayer.cpp
@@ -59,7 +59,7 @@ QuAudioPlayer* QuAudioPlayer::setVolume(const int volume)
     // qDebug().nospace() << "QuAudioPlayer::setVolume(" << volume << ")";
     m_volume = qBound(uiconst::MIN_VOLUME_QT, volume, uiconst::MAX_VOLUME_QT);
     if (m_player) {
-        m_player->audioOutput()->setVolume(m_volume);
+        soundfunc::setVolume(m_player, m_volume);
     }
     return this;
 }
@@ -113,7 +113,7 @@ QPointer<QWidget> QuAudioPlayer::makeWidget(Questionnaire* questionnaire)
     soundfunc::makeMediaPlayer(m_player);
     if (m_player) {
         m_player->setSource(QUrl(m_url));
-        m_player->audioOutput()->setVolume(m_volume);
+        setVolume(m_volume);
         connect(m_player.data(), &QMediaPlayer::mediaStatusChanged,
                 this, &QuAudioPlayer::mediaStatusChanged);
     } else {
