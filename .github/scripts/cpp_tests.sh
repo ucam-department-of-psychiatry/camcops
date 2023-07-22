1aa9a93096803b199fc20032b189ca8ece165019
Martin Burchell <mb2353@cam.ac.uk>
2023-07-22 17:11:23 +0100

    Build C++ tests outside the source tree

--- .github/scripts/cpp_tests.sh
+++ .github/scripts/cpp_tests.sh
@@ -7,8 +7,10 @@ cd eigen
 EIGEN_VERSION=3.4.0
 wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_VERSION}/eigen-${EIGEN_VERSION}.tar.gz
 tar xzf eigen-${EIGEN_VERSION}.tar.gz
-cd ${GITHUB_WORKSPACE}/tablet_qt/tests
-qmake
+cd ${GITHUB_WORKSPACE}
+mkdir build-qt5-tests
+cd build-qt5-tests
+qmake ../tablet_qt/tests
 make
 export QT_DEBUG_PLUGINS=1
 
