f1aeda59b4fbf5d9ce05e339552584d826dc3830
Martin Burchell <mb2353@cam.ac.uk>
2023-07-31 12:04:05 +0100

    Checkout before running script

--- .github/scripts/free_up_disk_space.sh
+++ .github/scripts/free_up_disk_space.sh
@@ -31,3 +31,5 @@ echo "some packages purged"
 sudo dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -nr | head
 df . -h
 sudo du /usr/ -hx -d 4 --threshold=1G | sort -hr | head
+
+sudo rm -rf ${GITHUB_WORKSPACE}/.git
