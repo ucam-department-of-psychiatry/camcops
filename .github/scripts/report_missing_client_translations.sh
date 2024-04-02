2ca102596e86b1498b457372e46621a5133fe777
Martin Burchell <mb2353@cam.ac.uk>
2024-04-02 23:44:23 +0100

    lupdate etc should already be on the path

--- .github/scripts/report_missing_client_translations.sh
+++ .github/scripts/report_missing_client_translations.sh
@@ -4,12 +4,7 @@
 
 set -euxo pipefail
 
-ls ${QT_ROOT_DIR}
-export QT_BIN_DIR=${QT_ROOT_DIR}/gcc_64/bin
-ls ${QT_BIN_DIR}
-export LCONVERT=${QT_BIN_DIR}/lconvert
-export LRELEASE=${QT_BIN_DIR}/lrelease
-export LUPDATE=${QT_BIN_DIR}/lupdate
+which lconvert
 
 PYTHON=${HOME}/venv/bin/python
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py all
