1109aa0b4167b97e4bed88d9f2332b2d65df0677
Martin Burchell <mb2353@cam.ac.uk>
2024-04-02 21:46:47 +0100

    Install Qt Linguist tools for translation workflow

--- .github/scripts/report_missing_client_translations.sh
+++ .github/scripts/report_missing_client_translations.sh
@@ -4,6 +4,13 @@
 
 set -euxo pipefail
 
+export QT6_BASE_DIR=${RUNNER_WORKSPACE}
+ls ${QT6_BASE_DIR}
+export QT6_BIN_DIR=${QT6_BASE_DIR}/gcc_64/bin
+export LCONVERT=${QT6_BIN_DIR}/lconvert
+export LRELEASE=${QT6_BIN_DIR}/lrelease
+export LUPDATE=${QT6_BIN_DIR}/lupdate
+
 PYTHON=${HOME}/venv/bin/python
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py all
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py missing
