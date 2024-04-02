97fbd82a2611603d40f0ab8b7110824aaa347d18
Martin Burchell <mb2353@cam.ac.uk>
2024-04-02 23:01:56 +0100

    Fix path to Qt in translations workflow

--- .github/scripts/report_missing_client_translations.sh
+++ .github/scripts/report_missing_client_translations.sh
@@ -4,13 +4,12 @@
 
 set -euxo pipefail
 
-export QT6_BASE_DIR=${RUNNER_WORKSPACE}/Qt
-ls ${QT6_BASE_DIR}
-export QT6_BIN_DIR=${QT6_BASE_DIR}/gcc_64/bin
-ls ${QT6_BIN_DIR}
-export LCONVERT=${QT6_BIN_DIR}/lconvert
-export LRELEASE=${QT6_BIN_DIR}/lrelease
-export LUPDATE=${QT6_BIN_DIR}/lupdate
+ls ${QT_ROOT_DIR}
+export QT_BIN_DIR=${QT_ROOT_DIR}/gcc_64/bin
+ls ${QT_BIN_DIR}
+export LCONVERT=${QT_BIN_DIR}/lconvert
+export LRELEASE=${QT_BIN_DIR}/lrelease
+export LUPDATE=${QT_BIN_DIR}/lupdate
 
 PYTHON=${HOME}/venv/bin/python
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py all
