4a353fb9f673a2effc09e7e0991974d0954786e4
Martin Burchell <mb2353@cam.ac.uk>
2024-04-03 10:06:33 +0100

    Dump Qt lib directory

--- .github/scripts/report_missing_client_translations.sh
+++ .github/scripts/report_missing_client_translations.sh
@@ -4,8 +4,6 @@
 
 set -euxo pipefail
 
-echo ${LD_LIBRARY_PATH}
-
 PYTHON=${HOME}/venv/bin/python
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py all
 ${PYTHON} ${GITHUB_WORKSPACE}/tablet_qt/tools/build_client_translations.py missing
