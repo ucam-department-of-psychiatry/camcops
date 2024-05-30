25a111fe17b4865657e7babf6d559af8d8625f17
Martin Burchell <mb2353@cam.ac.uk>
2024-05-30 21:32:12 +0100

    Test widget positioner centering

--- tablet_qt/tests/auto/qobjects/widgetpositioner/testwidgetpositioner.cpp
+++ tablet_qt/tests/auto/qobjects/widgetpositioner/testwidgetpositioner.cpp
@@ -30,6 +30,7 @@ class TestWidgetPositioner: public QObject
 
 private slots:
     void testOrientationChangeClipsToScreenIfTooBig();
+    void testOrientationChangeCentresWidget();
 
 };
 
@@ -48,12 +49,35 @@ void TestWidgetPositioner::testOrientationChangeClipsToScreenIfTooBig()
     QVERIFY(ok);
 
     QRect screen_geometry = dialog->screen()->availableGeometry();
-    qDebug() << "Screen size:";
-    qDebug() << screen_geometry;
 
     QCOMPARE(dialog->width(), screen_geometry.width());
     QCOMPARE(dialog->height(), screen_geometry.height());
+}
 
+
+void TestWidgetPositioner::testOrientationChangeCentresWidget()
+{
+    auto dialog = new QDialog();
+
+    // Something small to ensure it fits on the screen
+    const int dialog_width = 100;
+    const int dialog_height = 100;
+
+    dialog->resize(dialog_width, dialog_height);
+    dialog->move(0, 0);
+    auto positioner = new WidgetPositioner(dialog);
+
+    // The orientation actually gets ignored but we have to put in something
+    const bool ok = QMetaObject::invokeMethod(positioner, "orientationChanged",
+                                              Qt::PortraitOrientation);
+    QVERIFY(ok);
+
+    QRect screen_geometry = dialog->screen()->availableGeometry();
+
+    QCOMPARE(dialog->width(), dialog_width);
+    QCOMPARE(dialog->height(), dialog_height);
+    QCOMPARE(dialog->pos().x(), (screen_geometry.width() - dialog_width) /2);
+    QCOMPARE(dialog->pos().y(), (screen_geometry.height() - dialog_height) /2);
 }
 
 QTEST_MAIN(TestWidgetPositioner)
