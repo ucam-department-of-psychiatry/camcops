34d0cebf151eac8a8969f8ff487c2ade0606a768
Martin Burchell <mb2353@cam.ac.uk>
2024-06-03 22:07:29 +0100

    No need to download Eigen twice
    The build_qt.py script should download it for us

--- .github/scripts/cpp_tests.sh
+++ .github/scripts/cpp_tests.sh
@@ -2,12 +2,6 @@
 set -euxo pipefail
 export CAMCOPS_QT6_BASE_DIR=${RUNNER_WORKSPACE}
 cd ${CAMCOPS_QT6_BASE_DIR}
-mkdir -p eigen
-cd eigen
-EIGEN_VERSION_FILE=${GITHUB_WORKSPACE}/tablet_qt/eigen_version.txt
-EIGEN_VERSION=$(<$EIGEN_VERSION_FILE)
-wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://gitlab.com/libeigen/eigen/-/archive/${EIGEN_VERSION}/eigen-${EIGEN_VERSION}.tar.gz
-tar xzf eigen-${EIGEN_VERSION}.tar.gz
 cd ${GITHUB_WORKSPACE}
 mkdir build-qt6-tests
 cd build-qt6-tests
