1180a50fec28130eea79e15ebf558d62b8c80c09
Martin Burchell <mb2353@cam.ac.uk>
2024-06-04 16:32:19 +0100

    Check qmake configuration

--- .github/scripts/cpp_tests.sh
+++ .github/scripts/cpp_tests.sh
@@ -5,6 +5,7 @@ echo "CAMCOPS_QT6_BASE_DIR=${CAMCOPS_QT6_BASE_DIR}"
 cd ${GITHUB_WORKSPACE}
 mkdir build-qt6-tests
 cd build-qt6-tests
+${CAMCOPS_QT6_BASE_DIR}/qt_linux_x86_64_install/bin/qmake -query
 ${CAMCOPS_QT6_BASE_DIR}/qt_linux_x86_64_install/bin/qmake ../tablet_qt/tests
 make
 export QT_DEBUG_PLUGINS=1
