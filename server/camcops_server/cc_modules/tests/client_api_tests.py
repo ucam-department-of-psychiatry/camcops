6a4bfcef1a31f3ac15ec1e3b9bd4735cbe0cabb6
Martin Burchell <mb2353@cam.ac.uk>
2025-01-23 11:48:58 +0000

    Test validate patients API fails if which_idnum already seen [no ci]

--- server/camcops_server/cc_modules/tests/client_api_tests.py
+++ server/camcops_server/cc_modules/tests/client_api_tests.py
@@ -1160,3 +1160,31 @@ class ValidatePatientsTests(DemoRequestTestCase):
 
         self.assertIn(f"WARNING:{logger_name}", logging_cm.output[0])
         self.assertIn("Bad ID number type: 2", logging_cm.output[0])
+
+    def test_fails_if_which_idnum_already_seen(self) -> None:
+        self.post_dict[TabletParam.PATIENT_INFO] = json.dumps(
+            [
+                {
+                    f"{TabletParam.IDNUM_PREFIX}1": 12345,
+                    f"{TabletParam.IDNUM_PREFIX}01": 12345,
+                }
+            ]
+        )
+
+        self.req.fake_request_post_from_dict(self.post_dict)
+        self.req.valid_which_idnums = [1]
+
+        with self.assertLogs(level=logging.WARN) as logging_cm:
+            response = client_api(self.req)
+            reply_dict = get_reply_dict_from_response(response)
+
+        self.assertEqual(
+            reply_dict[TabletParam.SUCCESS], FAILURE_CODE, msg=reply_dict
+        )
+        logger_name = "camcops_server.cc_modules.client_api"
+
+        self.assertIn(f"WARNING:{logger_name}", logging_cm.output[0])
+        self.assertIn(
+            "More than one ID number supplied for ID number type 1",
+            logging_cm.output[0],
+        )
