ffe9f33907a5017d585c9b6d7550b6b0ee3ea529
Rudolf Cardinal <rudolf@pobox.com>
2025-03-06 09:54:20 +0000

    bump cardinal_pythonlib version

--- server/setup.py
+++ server/setup.py
@@ -67,7 +67,7 @@ INSTALL_REQUIRES = [
     # -------------------------------------------------------------------------
     "alembic==1.4.2",  # database migrations
     "asteval==0.9.25",  # safe-ish alternative to eval
-    "cardinal_pythonlib==1.1.25",  # RNC libraries
+    "cardinal_pythonlib==2.0.2",  # RNC libraries
     "celery==5.3.6",  # background tasks
     "colander==1.7.0",  # serialization/deserialization from web forms
     "CherryPy==18.6.0",  # web server
