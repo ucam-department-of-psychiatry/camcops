ba1e5c9164090029fc3099cf4ca50d288bb120a2
Martin Burchell <mb2353@cam.ac.uk>
2025-03-25 10:56:51 +0000

    EPDS typing

--- server/camcops_server/tasks/epds.py
+++ server/camcops_server/tasks/epds.py
@@ -27,7 +27,7 @@ camcops_server/tasks/epds.py
 
 """
 
-from typing import Any, List, Type
+from typing import Any, List, Type, Union
 
 from cardinal_pythonlib.stringfunc import strseq
 from sqlalchemy.sql.sqltypes import Integer
@@ -121,7 +121,7 @@ class Epds(
     def is_complete(self) -> bool:
         return self.all_fields_not_none(self.TASK_FIELDS)
 
-    def total_score(self) -> int:
+    def total_score(self) -> Union[int, float]:
         return self.sum_fields(self.TASK_FIELDS)
 
     def get_task_html(self, req: CamcopsRequest) -> str:
