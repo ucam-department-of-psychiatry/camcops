91b101c1c87ece6b2584610087820ce147cba349
Martin Burchell <mb2353@cam.ac.uk>
2025-03-25 10:58:54 +0000

    CESDR typing

--- server/camcops_server/tasks/cesdr.py
+++ server/camcops_server/tasks/cesdr.py
@@ -25,7 +25,7 @@ camcops_server/tasks/cesdr.py
 
 """
 
-from typing import Any, List, Type
+from typing import Any, List, Type, Union
 
 from cardinal_pythonlib.classes import classproperty
 from cardinal_pythonlib.stringfunc import strseq
@@ -150,7 +150,7 @@ class Cesdr(
             and self.field_contents_valid()
         )
 
-    def total_score(self) -> int:
+    def total_score(self) -> Union[int, float]:
         return self.sum_fields(self.SCORED_FIELDS) - self.count_where(
             self.SCORED_FIELDS, [self.FREQ_DAILY_2_WEEKS]
         )
