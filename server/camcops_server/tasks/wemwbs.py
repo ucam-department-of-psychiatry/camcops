9f8339a1989844e4399a0c59d90aaba458e50094
Martin Burchell <mb2353@cam.ac.uk>
2025-03-25 10:35:03 +0000

    WEMWBS typing

--- server/camcops_server/tasks/wemwbs.py
+++ server/camcops_server/tasks/wemwbs.py
@@ -25,7 +25,7 @@ camcops_server/tasks/wemwbs.py
 
 """
 
-from typing import Any, List, Type
+from typing import Any, List, Type, Union
 
 from cardinal_pythonlib.stringfunc import strseq
 from sqlalchemy.sql.sqltypes import Integer
@@ -142,7 +142,7 @@ class Wemwbs(
             )
         ]
 
-    def total_score(self) -> int:
+    def total_score(self) -> Union[int, float]:
         return self.sum_fields(self.TASK_FIELDS)
 
     def get_task_html(self, req: CamcopsRequest) -> str:
@@ -318,7 +318,7 @@ class Swemwbs(
             )
         ]
 
-    def total_score(self) -> int:
+    def total_score(self) -> Union[int, float]:
         return self.sum_fields(self.TASK_FIELDS)
 
     def get_task_html(self, req: CamcopsRequest) -> str:
