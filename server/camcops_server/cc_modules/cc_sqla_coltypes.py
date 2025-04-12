9943ea229d7f80a464b8ee63d313f54ad27b0984
Martin Burchell <mb2353@cam.ac.uk>
2025-04-12 12:09:03 +0100

    Add permitted value checker for 1-10

--- server/camcops_server/cc_modules/cc_sqla_coltypes.py
+++ server/camcops_server/cc_modules/cc_sqla_coltypes.py
@@ -1433,7 +1433,7 @@ ONE_TO_SIX_CHECKER = PermittedValueChecker(minimum=1, maximum=6)
 ONE_TO_SEVEN_CHECKER = PermittedValueChecker(minimum=1, maximum=7)
 ONE_TO_EIGHT_CHECKER = PermittedValueChecker(minimum=1, maximum=8)
 ONE_TO_NINE_CHECKER = PermittedValueChecker(minimum=1, maximum=9)
-
+ONE_TO_TEN_CHECKER = PermittedValueChecker(minimum=1, maximum=10)
 
 # =============================================================================
 # camcops_column: provides extra functions over Column.
