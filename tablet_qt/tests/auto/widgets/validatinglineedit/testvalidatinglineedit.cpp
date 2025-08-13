f1b211c0bdfc242dd3e99e841fd4bc0f1f75ac71
Martin Burchell <mb2353@cam.ac.uk>
2025-08-13 10:13:45 +0100

    Initial ValidatingLineEdit tests

---
+++ tablet_qt/tests/auto/widgets/validatinglineedit/testvalidatinglineedit.cpp
@@ -0,0 +1,68 @@
+/*
+    Copyright (C) 2012, University of Cambridge, Department of Psychiatry.
+    Created by Rudolf Cardinal (rnc1001@cam.ac.uk).
+
+    This file is part of CamCOPS.
+
+    CamCOPS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CamCOPS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CamCOPS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <QHBoxLayout>
+#include <QtTest/QtTest>
+#include <QVBoxLayout>
+
+#include "widgets/validatinglineedit.h"
+
+class TestValidatingLineEdit : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testHasHorizontalLayout();
+    void testHasVerticalLayout();
+};
+
+void TestValidatingLineEdit::testHasVerticalLayout()
+{
+    QValidator* validator = nullptr;
+    const bool allow_empty = false;
+    const bool read_only = false;
+    const bool delayed = false;
+    const bool vertical = true;
+
+    auto line_edit = new ValidatingLineEdit(
+        validator, allow_empty, read_only, delayed, vertical
+    );
+
+    QVERIFY(qobject_cast<QVBoxLayout*>(line_edit->layout()) != nullptr);
+}
+
+void TestValidatingLineEdit::testHasHorizontalLayout()
+{
+    QValidator* validator = nullptr;
+    const bool allow_empty = false;
+    const bool read_only = false;
+    const bool delayed = false;
+    const bool vertical = false;
+
+    auto line_edit = new ValidatingLineEdit(
+        validator, allow_empty, read_only, delayed, vertical
+    );
+
+    QVERIFY(qobject_cast<QHBoxLayout*>(line_edit->layout()) != nullptr);
+}
+
+QTEST_MAIN(TestValidatingLineEdit)
+
+#include "testvalidatinglineedit.moc"
---
+++ tablet_qt/tests/auto/widgets/validatinglineedit/validatinglineedit.pro
@@ -0,0 +1,40 @@
+CONFIG += testcase
+QT += testlib
+QT += widgets
+QT -= gui
+
+TARGET = bin/test_validatinglineedit
+
+RESOURCES += \
+    $$CAMCOPS_SRC/test.qrc
+
+SOURCES += \
+    testvalidatinglineedit.cpp \
+    $$CAMCOPS_SRC/common/cssconst.cpp \
+    $$CAMCOPS_SRC/common/dpi.cpp \
+    $$CAMCOPS_SRC/common/platform.cpp \
+    $$CAMCOPS_SRC/common/uiconst.cpp \
+    $$CAMCOPS_SRC/lib/css.cpp \
+    $$CAMCOPS_SRC/lib/errorfunc.cpp \
+    $$CAMCOPS_SRC/lib/filefunc.cpp \
+    $$CAMCOPS_SRC/lib/timerfunc.cpp \
+    $$CAMCOPS_SRC/lib/widgetfunc.cpp \
+    $$CAMCOPS_SRC/qobjects/focuswatcher.cpp \
+    $$CAMCOPS_SRC/widgets/validatinglineedit.cpp
+
+HEADERS += \
+    $$CAMCOPS_SRC/common/cssconst.h \
+    $$CAMCOPS_SRC/common/dpi.h \
+    $$CAMCOPS_SRC/common/platform.h \
+    $$CAMCOPS_SRC/common/uiconst.h \
+    $$CAMCOPS_SRC/lib/css.h \
+    $$CAMCOPS_SRC/lib/errorfunc.h \
+    $$CAMCOPS_SRC/lib/filefunc.h \
+    $$CAMCOPS_SRC/lib/timerfunc.h \
+    $$CAMCOPS_SRC/lib/widgetfunc.h \
+    $$CAMCOPS_SRC/qobjects/focuswatcher.h \
+    $$CAMCOPS_SRC/widgets/validatinglineedit.h
+
+INCLUDEPATH += $$CAMCOPS_SRC
+
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
