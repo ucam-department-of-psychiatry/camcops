15ec7d5c2a03a3f60633d7261e3804f74e233656
Martin Burchell <mb2353@cam.ac.uk>
2026-08-18 06:41:47 +0100

    Fix tests

--- server/camcops_server/tasks/tests/aq10_tests.py
+++ server/camcops_server/tasks/tests/aq10_tests.py
@@ -30,56 +30,56 @@ from unittest import TestCase
 from camcops_server.tasks.aq10 import Aq10
 
 
-class AqTests(TestCase):
+class Aq10Tests(TestCase):
     AGREE_SCORING_QUESTIONS = [1, 7, 8, 10]
 
     DISAGREE_SCORING_QUESTIONS = [2, 3, 4, 5, 6, 9]
 
-    ALL_QUESTIONS = range(1, 50 + 1)
+    ALL_QUESTIONS = range(1, 10 + 1)
 
     DEFINITELY_AGREE = 0
     DEFINITELY_DISAGREE = 3
 
     def test_max_score_is_10(self) -> None:
-        aq = Aq10()
+        aq10 = Aq10()
         for q_num in self.AGREE_SCORING_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_AGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_AGREE)
 
         for q_num in self.DISAGREE_SCORING_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_DISAGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_DISAGREE)
 
-        self.assertEqual(aq.score(), 50)
+        self.assertEqual(aq10.score(), 10)
 
     def test_min_score_is_0(self) -> None:
-        aq = Aq10()
+        aq10 = Aq10()
 
         for q_num in self.AGREE_SCORING_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_DISAGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_DISAGREE)
 
         for q_num in self.DISAGREE_SCORING_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_AGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_AGREE)
 
-        self.assertEqual(aq.score(), 0)
+        self.assertEqual(aq10.score(), 0)
 
     def test_score_is_none_if_any_none(self) -> None:
-        aq = Aq10()
+        aq10 = Aq10()
 
         for q_num in self.ALL_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_AGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_AGREE)
 
-        aq.q1 = None  # type: ignore[attr-defined]
+        aq10.q1 = None  # type: ignore[attr-defined]
 
-        self.assertIsNone(aq.score())
+        self.assertIsNone(aq10.score())
 
     def test_incomplete_when_answers_missing(self) -> None:
-        aq = Aq10()
+        aq10 = Aq10()
 
-        self.assertFalse(aq.is_complete())
+        self.assertFalse(aq10.is_complete())
 
     def test_complete_when_all_answered(self) -> None:
-        aq = Aq10()
+        aq10 = Aq10()
 
         for q_num in self.ALL_QUESTIONS:
-            setattr(aq, f"q{q_num}", self.DEFINITELY_AGREE)
+            setattr(aq10, f"q{q_num}", self.DEFINITELY_AGREE)
 
-        self.assertTrue(aq.is_complete())
+        self.assertTrue(aq10.is_complete())
